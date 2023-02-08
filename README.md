48079f6f95f751c42e55c8e883d0da5cbf319543
sutha <sutha@kurinchilion.com>
2023-02-08 18:05:44 -0500

    base changes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# BasicVuex
-Sample Vue app to demonstrate Vuex state store
+### BasicVuex
+* Sample Vue app to demonstrate Vuex state store
+* We will use Vue 3 for this exercise
+  
+### Vue CLI instalation
+* Reference: https://cli.vuejs.org/guide/installation.html
+
+
+```
+$ vue create sample-vuex
+$ npm run serve
+
+```
+* Visit Local:   http://localhost:8081/  to view the app locally
+* 
---
+++ sample-vuex/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="counter">{{ counter }}</div>
+  <div class="buttons">
+    <button @click="decreaseCounter">-</button>
+    <button @click="increaseCounter">+</button>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter(){
+      this.counter++;
+    },
+    decreaseCounter(){
+      this.counter--;
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+div{
+  margin-bottom: 18px;
+}
+.counter{
+  font-size: 80px;
+}
+.buttons button{
+  font-size: 48px;
+  width: 100px;
+  margin: 0 10px;
+
+}
+</style>
